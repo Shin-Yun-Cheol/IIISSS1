487b1b9ea4e79ccf1714d3a123ab6d83be4a23d9
Shin-Yun-Cheol <yptlsdbscjf@naver.com>
2021-07-27 01:49:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/iiisss1/LastActivity.kt
@@ -0,0 +1,12 @@
+package com.example.iiisss1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LastActivity(intent2: Intent) : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last)
+    }
+}
